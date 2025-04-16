a3db6b452504fc1e6ec5ae4caccdcd785c7d6d0e
KingKal-prg <k648k@outlook.com>
2025-04-16 08:39:15 -0400

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -2,7 +2,7 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('app-cache').then((cache) => {
       return cache.addAll([
-        '/Ai.html',
+        '/browser.html',
         '/manifest.json',
         '/icon.png'
       ]);
